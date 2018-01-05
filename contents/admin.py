19dd1eae9518e6fee10c2688db6dbef1595e400c
Ian De La Cruz <ridlc10@gmail.com>
2018-01-05 23:15:46 +0800

    Add admin

--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Content, Property, PropertyImage
 
-# Register your models here.
+
+class ContentAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'page',
+        'content'
+    ]
+admin.site.register(Content, ContentAdmin)
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'completion_status',
+        'unit_type',
+        'location'
+    ]
+admin.site.register(Property, PropertyAdmin)
+
+class PropertyImageAdmin(admin.ModelAdmin):
+    list_display = [
+        'mw_property',
+        'is_primary'
+    ]
+admin.site.register(PropertyImage, PropertyImageAdmin)
