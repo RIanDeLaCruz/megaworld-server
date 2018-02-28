8ef4c490c9bb8cedd7a60dbbec2c4fe48de8a16b
Ian De La Cruz <ridlc10@gmail.com>
2018-02-28 21:01:38 +0800

    Fix sms sending

--- contents/views.py
+++ contents/views.py
@@ -13,13 +13,18 @@ class ChikkaView(APIView):
 
         api_key = 'b30ae54fd061de0faa02582e765b52a8'
 
+        message = "From {}:\n\
+        Regarding: {} \n\
+        {}".format(request.POST['contact_number'], request.POST['property'], request.POST['message'])
+        print ( message )
+
         params = (
                 ('apikey', api_key),
-                ('message', request.POST['message']),
+                ('message', message),
                 ('number', '09267398210')
         )
 
-        path = 'https://semaphore.co/api/v4/messages?' + urllib.urlencode(params)
+        path = 'https://semaphore.co/api/v4/messages?' + urllib.parse.urlencode(params)
         requests.post(path)
 
-        return Response({'success', True}, status=status.HTTP_200_OK)
+        return Response({'success', True, 'message', message}, status=status.HTTP_200_OK)
--- mw/urls.py
+++ mw/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from contents.views import ChikkaView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^send_sms', ChikkaView.as_view())
 ]
