201fa9c980505336b15ee58829bcbaf5ffb14540
Ian De La Cruz <ridlc10@gmail.com>
2018-02-28 20:43:01 +0800

    Add SMS Sending API

--- contents/views.py
+++ contents/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import requests
+import datetime
+import secrets
+import sys, urllib
 
-# Create your views here.
+
+class ChikkaView(APIView):
+    def post(self, request):
+
+        api_key = 'b30ae54fd061de0faa02582e765b52a8'
+
+        params = (
+                ('apikey', api_key),
+                ('message', request.POST['message']),
+                ('number', '09267398210')
+        )
+
+        path = 'https://semaphore.co/api/v4/messages?' + urllib.urlencode(params)
+        requests.post(path)
+
+        return Response({'success', True}, status=status.HTTP_200_OK)
